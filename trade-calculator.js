303f08fa5025dfa6b19b869a7250eff18bfc1b6a
kevinmoua87 <kmoua87@hotmail.com>
2025-09-18 01:52:30 -0700

    On branch main  Second Commit  	Added some styling to the website  Changes to be committed: 	modified:   index.html 	modified:   trade-calculator.css 	modified:   trade-calculator.js

---
+++ trade-calculator.js
@@ -0,0 +1,19 @@
+
+
+function calc(){
+    const baseInput = document.getElementById("Base");
+    const rateInput = document.getElementById("Rate");
+    const contInput = document.getElementById("Contribution");
+    const numTradeInput = document.getElementById("NumTrades");
+    let y = 0;
+
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Your code here will execute once the DOM is ready for manipulation
+  console.log('DOM is loaded and ready!');
+
+  calc();
+});
+
